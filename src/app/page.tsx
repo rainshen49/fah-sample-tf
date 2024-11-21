9c8f01e0f21e1be930036c83516630b402f3161a
Lingkai Shen <lingkai@google.com>
2024-11-21 10:36:03 -0500

    Tickle build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             >
               Cloud Run
             </Link>
-            , a fully managed container that scales up and down with demand.
+            , a fully managed container that scales up and down with demand. Isn't that cool?
             Visit{" "}
             <Link href="/ssr">
               <code>/ssr</code>
